b6be7a1f6d6947cdad067086b0492fd58a33cd0c
UBS-Khan <umair.salman@xloopdigital.com>
2023-04-11 20:35:33 +0500

    AC 1 - Test Passed!

--- src/Cart.js
+++ src/Cart.js
@@ -1,3 +1,4 @@
 module.exports = class Cart {
-
+    items = [];
+    checkItems = () => this.items.length
 }
--- test/Cart.test.js
+++ test/Cart.test.js
@@ -1,8 +1,10 @@
+const Cart = require('../src/Cart.js')
 test('AC1',()=>{
     //Arrange
     const assert = 0;
+    const cart = new Cart();
     //Act
-    const result = cart.items
+    const result = cart.checkItems();
     //Assert
-    expect(result).toBe(assert)
+    expect(result).toBe(assert);
 })
