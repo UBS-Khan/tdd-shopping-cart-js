57c1c25d2dd4658480e1379957de2a9141db7874
UBS-Khan <umair.salman@xloopdigital.com>
2023-04-12 05:37:22 +0500

    AC$ - Test Failed!

--- test/Cart.test.js
+++ test/Cart.test.js
@@ -36,3 +36,17 @@ test('AC3',()=>{
     //Assert
     expect(result).toEqual(assert);
 });
+
+test('AC4',()=>{
+    //Arrange
+    const assert = ['Mango - x2 - $8', 'Banana - x4 - $8'];
+    const cart = new Cart();
+    const itemM = new Item("Mango", 4, false, 2)
+    const itemB = new Item("Banana", 2, false, 4)
+    cart.addItem(itemM)
+    cart.addItem(itemB)
+    //Act
+    const result = cart.itemizedList(cart.items);
+    //Assert
+    expect(result).toEqual(assert);
+});
