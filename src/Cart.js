f64ed56316f855964d8aadbef65f3bb7d3a179e7
UBS-Khan <umair.salman@xloopdigital.com>
2023-04-12 05:07:48 +0500

    AC1 - Test Failed!

--- src/Cart.js
+++ src/Cart.js
@@ -1,18 +1,2 @@
 module.exports = class Cart {
-    items = [];
-    totalPrice = 0;
-
-    clearList = () => this.items = [];
-
-    checkItems = () => this.items.length;
-
-    addItem = (item) => {
-        let itemPrice = item.price * item.quantity;
-        this.items.push(item);
-        this.totalPrice += itemPrice;
-    }
-
-    itemQuantities = (items) => {
-        return items.map(item => `${item.name} - x${item.quantity}`)
-    }
 }
--- test/Cart.test.js
+++ test/Cart.test.js
@@ -1,41 +1,9 @@
-const Cart = require('../src/Cart.js')
-const Item = require('../src/Item.js')
-
 test('AC1',()=>{
     //Arrange
     const assert = 0;
     const cart = new Cart();
     //Act
-    const result = cart.checkItems();
+    const result = cart.items;
     //Assert
-    expect(result).toBe(assert);
+    expect(result),toEqual(assert);
 })
-
-test('AC2',()=>{
-    //Arrange
-    const assert = 50;
-    const cart = new Cart();
-    cart.clearList();
-    const item = new Item("Handbag", 25, false, 2);
-    cart.addItem(item)
-    //Act
-    const result = cart.totalPrice;
-    //Assert
-    expect(result).toBe(assert);
-})
-
-test('AC3',()=>{
-    //Arrange
-    const assert = ['Handbag - x2', 'Gum - x5'];
-    const cart = new Cart();
-    cart.clearList();
-    const item = new Item("Handbag", 25, false, 2);
-    const item2 = new Item("Gum", 5, false, 5);
-    cart.addItem(item)
-    cart.addItem(item2)
-    //Act
-    const result = cart.itemQuantities(cart.items);
-    //Assert
-    expect(result).toEqual(assert);
-})
-
